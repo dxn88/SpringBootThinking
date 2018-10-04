2ed2e616c0cd37fe5fede172258409c0f1bff3e2
daixunan <daixunan@gmail.com>
2018-10-05 01:22:37 +0800

    增加第一次提交

--- src/com/jd/OuterClass.java
+++ src/com/jd/OuterClass.java
@@ -1,14 +1,38 @@
 package com.jd;
 
+import com.sun.deploy.util.StringUtils;
+
 /**
  * @Auther: daixunan
  * @Date: 2018/10/5
  */
 public class OuterClass {
+
+    private static final char PACKAGE_SEPARATOR = '.';
+    private static final char INNER_CLASS_SEPARATOR = '$';
+
+    private static final String innerClassPath = "om.jd.OuterClass.InnerClass";
+
     public static void main(String[] args) {
         // 外部类和内部类用$符号链接
         System.out.println("InnerClass.class.getName() = " + InnerClass.class.getName());
+
+        String innerClass = convert2InnerClassPath(innerClassPath);
+
+        System.out.println("innerClass = " + innerClass);
+    }
+
+    private static String convert2InnerClassPath(String path) {
+        int lastDotIndex = path.lastIndexOf(PACKAGE_SEPARATOR);
+        if (lastDotIndex != -1) {
+            String innerClassName = path.substring(0, lastDotIndex) + INNER_CLASS_SEPARATOR + path.substring(lastDotIndex + 1);
+            return innerClassName;
+        }
+
+        return "";
     }
 
     public static class InnerClass{}
+
+
 }
