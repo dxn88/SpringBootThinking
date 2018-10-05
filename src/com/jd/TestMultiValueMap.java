5fbd40e72906b334373ddae6ac2f3ee29a76af5a
daixunan <daixunan@gmail.com>
2018-10-05 13:35:33 +0800

    增加多值map

---
+++ src/com/jd/TestMultiValueMap.java
@@ -0,0 +1,32 @@
+package com.jd;
+
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @Auther: daixunan
+ * @Date: 2018/10/5
+ */
+public class TestMultiValueMap {
+    public static void main(String[] args) {
+        // 一个key对应多个value
+        MultiValueMap multiValueMap = new LinkedMultiValueMap();
+
+        multiValueMap.add("hello", "world");
+        multiValueMap.add("hello", "China");
+
+        Set<Map.Entry<Object, List<Object>> > set = multiValueMap.entrySet();
+        for (Map.Entry<Object, List<Object>> entry : set) {
+
+            for(Object obj : entry.getValue()){
+                System.out.println("obj = " + obj);
+            }
+
+        }
+
+    }
+}
