1858f68221adb56b484b3cc5b9843db3bad772a1
daixunan <daixunan@gmail.com>
2018-10-05 20:25:47 +0800

    增加泛型获得

--- src/com/jd/TestResolvableType.java
+++ src/com/jd/TestResolvableType.java
@@ -11,12 +11,12 @@ import org.springframework.core.ResolvableType;
 // 提供对泛型的更强大操作,包括对字段的泛型信息
 public class TestResolvableType implements AA<String> {
     public static void main(String[] args) {
-
         ResolvableType resolvableType = ResolvableType.forClass(TestResolvableType.class);
         System.out.println(resolvableType);
         Class<?> resolve = resolvableType.getInterfaces()[0].getGeneric(0).resolve();
         System.out.println("resolve = " + resolve);
     }
+
 }
 
 interface AA<T>{}
