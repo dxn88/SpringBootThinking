cece25b6ab6d1a89cb6c2e14ae721a9860b2ae41
daixunan <daixunan@gmail.com>
2018-10-05 19:17:09 +0800

    增加main类方法

---
+++ src/com/jd/TestConstructor.java
@@ -0,0 +1,23 @@
+package com.jd;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @Auther: daixunan
+ * @Date: 2018/10/5
+ */
+
+// 反射创建对象
+public class TestConstructor {
+    public static void main(String[] args) {
+        Class<?>[] types = new Class<?>[] { SpringApplication.class, String[].class };
+//        List<T> instances = new ArrayList<>(names.size());
+//        Class<?> instanceClass = ClassUtils.forName(name, classLoader);
+//        Assert.isAssignable(type, instanceClass);
+//        Constructor<?> constructor = instanceClass
+//                .getDeclaredConstructor(parameterTypes);
+//        T instance = (T) BeanUtils.instantiateClass(constructor, args);
+//        instances.add(instance);
+
+    }
+}
