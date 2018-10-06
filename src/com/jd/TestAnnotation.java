58914ea537923b7d53c8929eda9a6a672ccf9051
daixunan <daixunan@gmail.com>
2018-10-06 22:41:13 +0800

    增加Annotation

--- src/com/jd/TestAnnotation.java
+++ src/com/jd/TestAnnotation.java
@@ -3,16 +3,28 @@ package com.jd;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.stereotype.Component;
 
+import java.util.jar.Attributes;
+
 /**
  * @Auther: daixunan
  * @Date: 2018/10/6
  */
+
+//AnnotationConfigUtils
 public class TestAnnotation {
 
     public static void main(String[] args) {
+        String name = "com.jd";
+        Package aPackage = Package.getPackage(name);
+        Attributes.Name name1 = new Attributes.Name("");
+
+        System.out.println("aPackage = " + aPackage.getImplementationTitle());
+        System.out.println("aPackage.getName() = " + aPackage.getName());
         Component annotation = AnnotationUtils.findAnnotation(TestLoadResource.class, Component.class);
         if (annotation != null) {
             System.out.println("annotation = " + annotation);
         }
     }
+
+
 }
