f4738a185d1ad75c1db1202857760fdfc6742597
daixunan <daixunan@gmail.com>
2018-10-07 16:51:22 +0800

    增加Annotation

--- src/com/jd/eureka/TestEureka.java
+++ src/com/jd/eureka/TestEureka.java
@@ -1,9 +1,9 @@
-package com.jd.eruka;
+package com.jd.eureka;
 
 /**
  * @Auther: daixunan
  * @Date: 2018/10/7
  * @Data 参考资料 https://blog.csdn.net/yehuang_0801/article/details/76055497
  */
-public class TestEruka {
+public class TestEureka {
 }
