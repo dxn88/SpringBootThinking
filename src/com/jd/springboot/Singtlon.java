5faf33fb633f87208d7b3d9455598b22756d6326
daixunan <346972070@qq.com>
2018-10-21 17:24:27 +0800

    增加单利模式 测试

--- src/com/jd/springboot/Singtlon.java
+++ src/com/jd/springboot/Singtlon.java
@@ -9,13 +9,13 @@ import java.util.List;
  */
 public class Singtlon {
 
+    private static Singtlon singtlon = new Singtlon();
     /**
      *  非常重要 所有静态资源都要放在单利模式的前面去加载 ************
      *  如果把singtlon放上面将会报错
      */
 
     private static List<String> strings = new ArrayList<>();
-    private static Singtlon singtlon = new Singtlon();
 
     public Singtlon() {
         strings.add("hello");
