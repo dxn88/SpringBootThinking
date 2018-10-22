c7eb34bcb3a4d61419541eef54a9c08ab074d0da
daixunan <346972070@qq.com>
2018-10-22 11:08:06 +0800

    增加logger

---
+++ src/com/jd/spring/TestLogger.java
@@ -0,0 +1,17 @@
+package com.jd.spring;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * @Auther: daixunan
+ * @Date: 2018/10/22
+ */
+
+/**
+ *  可以用lombok的@Sl4f 去代替此方案，此方案比用*.class要好
+ */
+public class TestLogger {
+    protected final Log logger = LogFactory.getLog(getClass());
+
+}
