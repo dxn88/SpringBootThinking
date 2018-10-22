251eb717ee6f572d02e7d1b71c02085bcdcfabbb
daixunan <346972070@qq.com>
2018-10-22 14:57:16 +0800

    增加日志拼接 节省资源

--- src/com/jd/spring/TestLogger.java
+++ src/com/jd/spring/TestLogger.java
@@ -11,10 +11,21 @@ import org.apache.commons.logging.LogFactory;
 /**
  *  可以用lombok的@Sl4f 去代替此方案，此方案比用*.class要好
  *  代码来源SimpleAliasRegistry
+ *  参考资料 https://blog.csdn.net/gongjds/article/details/78476652
  */
 public class TestLogger {
-    protected final Log logger = LogFactory.getLog(getClass());
+    private final Log logger = LogFactory.getLog(getClass());
 
+    public static void main(String[] args) {
+
+    }
+
+    private void print() {
+        // 可以节省字符串的拼接， 虽然在方法里面已经进行判断是否打印日志
+        if (logger.isInfoEnabled()) {
+            logger.info("logger " + "test" );
+        }
+    }
 }
 
 
