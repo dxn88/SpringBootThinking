732b2ea0585ebbbab38b824cbd0becb89e1034e4
daixunan <346972070@qq.com>
2018-10-22 10:30:07 +0800

    增加AutoCloseable 接口测试

--- src/com/jd/springboot/TestAutoCloseable.java
+++ src/com/jd/springboot/TestAutoCloseable.java
@@ -4,6 +4,10 @@ package com.jd.springboot;
  * @Auther: daixunan
  * @Date: 2018/10/22
  */
+
+/**
+ *  在try中使用必须继承AutoCloseable接口，并且在结束时会自动调用close()
+ */
 public class TestAutoCloseable implements AutoCloseable{
 
 
