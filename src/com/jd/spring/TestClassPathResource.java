c25b0ea421cdbd6504555a986ba7c8edf19c904a
daixunan <346972070@qq.com>
2018-10-22 15:09:44 +0800

    增加FileCopyUtil打印文件

--- src/com/jd/spring/TestClassPathResource.java
+++ src/com/jd/spring/TestClassPathResource.java
@@ -1,10 +1,10 @@
 package com.jd.spring;
 
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.FileCopyUtils;
 
-import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 /**
  * @Auther: daixunan
@@ -13,16 +13,10 @@ import java.io.InputStreamReader;
 public class TestClassPathResource {
     public static void main(String[] args) throws IOException {
         ClassPathResource classPathResource = new ClassPathResource("bean.xml");
+        File file = classPathResource.getFile();
+        byte[] bytes = FileCopyUtils.copyToByteArray(file);
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(classPathResource.getInputStream(), "UTF-8")); // 实例化输入流，并获取网页代码
-        String s; // 依次循环，至到读的值为空
-        StringBuilder sb = new StringBuilder();
-        while ((s = reader.readLine()) != null) {
-            sb.append(s);
-        }
-        reader.close();
-        String str = sb.toString();
+        System.out.println("new String(bytes) = " + new String(bytes));
 
-        System.out.println("str = " + str);
     }
 }
