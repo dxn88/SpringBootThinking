f0ee29172b242ba18b7a05e67ae7c3838eb786ad
daixunan <346972070@qq.com>
2018-10-22 13:50:29 +0800

    增加 ：：用法

--- src/com/jd/spring/TestLamba.java
+++ src/com/jd/spring/TestLamba.java
@@ -19,25 +19,32 @@ public class TestLamba {
 
     public static void main(String[] args) {
         final Test t = new Test();
-        Worker worker = Test::print;
-        worker.process(t);
+        Worker worker = t::print;
+        t.num++;
+        worker.process("hello");
+
+//        第二种用法 此用法传入的参数必须是 Test类型
+
+//        Worker worker1 = Test::print;
+
     }
 
-   public void   print() {
+   public void print() {
         System.out.println("TestLamba.print");
 
     }
 
     public static class Test{
-      public void print(){
-          System.out.println("Test.print");
+        public int num;
+      public void print( String s){
+          System.out.println(s+num);
       }
     }
 
 
     @FunctionalInterface
     interface Worker{
-        void process( Test str);
+        void process(String str);
     }
 
 
