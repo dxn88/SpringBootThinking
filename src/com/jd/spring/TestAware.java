5c5817ac3377d12e060864eb3b31db0a013bf6e6
daixunan <346972070@qq.com>
2018-10-23 17:30:50 +0800

    spring加载aware对象的原理

---
+++ src/com/jd/spring/TestAware.java
@@ -0,0 +1,40 @@
+package com.jd.spring;
+
+/**
+ * @Auther: daixunan
+ * @Date: 2018/10/23
+ */
+
+/**
+ * spring 加载bean的aware原理
+ */
+public class TestAware {
+
+    public static void main(String[] args) {
+        Spring spring = new Spring();
+//        ResourceAware resourceAware = (aware)->{};
+        spring.init(new MyObj());
+    }
+}
+
+class MyObj implements ResourceAware {
+    @Override
+    public void setResourceAware(String aware) {
+        System.out.println("aware = " + aware);
+    }
+}
+
+@FunctionalInterface
+interface ResourceAware{
+    void setResourceAware(String aware);
+}
+
+class Spring{
+
+    public void init(Object bean) {
+
+        if (bean instanceof ResourceAware) {
+            ((ResourceAware)bean).setResourceAware("ResourceAware");
+        }
+    }
+}
