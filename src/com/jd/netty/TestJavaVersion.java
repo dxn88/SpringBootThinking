fd23b0e5994e23f8ccecdec3abc6f8c1f1291e94
daixunan <346972070@qq.com>
2018-11-13 11:34:27 +0800

    加入异常测试

--- src/com/jd/netty/TestJavaVersion.java
+++ src/com/jd/netty/TestJavaVersion.java
@@ -1,8 +1,12 @@
 package com.jd.netty;
 
+import com.sun.xml.internal.ws.api.model.CheckedException;
 import sun.misc.Unsafe;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
+
+import static sun.misc.Unsafe.getUnsafe;
 
 /**
  * @author: <a href="mailto:daixunan@jd.com">戴续楠</a>
@@ -11,6 +15,7 @@ import java.lang.reflect.Field;
 public class TestJavaVersion {
 
     static Unsafe UNSAFE;
+    private String name = "dxn";
 
     static {
         try {
@@ -23,10 +28,50 @@ public class TestJavaVersion {
     }
 
 
-    public static void main(String[] args) throws InstantiationException {
+    public static void main(String[] args) throws Exception {
         TestJavaVersion instance = (TestJavaVersion) UNSAFE.allocateInstance(TestJavaVersion.class);
         int version = instance.majorVersionFromJavaSpecificationVersion();
         System.out.println("i = " + version);
+
+        instance.print();
+        Field name = instance.getClass().getDeclaredField("name");
+        UNSAFE.putObject(instance, UNSAFE.objectFieldOffset(name), "huanghui");
+        instance.print();
+
+        getUnsafe().throwException(new NoSuchFieldException());
+    }
+
+
+    static void doThrow(Exception e) {
+       doThrow0(e);
+    }
+
+    static <E extends Exception>
+    void doThrow0(Exception e) throws E {
+        throw (E) e;
+    }
+
+    public void print() throws Exception {
+
+        System.out.println("name = " + name);
+        throw new Exception("haha");
+    }
+
+    public void testPrint(){
+        try {
+            print();
+        } catch (Exception e) {
+//            getUnsafe().throwException(new IOException(e));
+            throw new RuntimeException("wrapper");
+        }
+    }
+
+    public void TestUnsafeExp() {
+        try {
+            testPrint();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public int majorVersionFromJavaSpecificationVersion() {
