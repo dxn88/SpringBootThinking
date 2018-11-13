fe3dbe13f10d8d212b95aecf620108a830877e47
daixunan <346972070@qq.com>
2018-11-13 14:15:22 +0800

    增加Object工厂

---
+++ src/com/jd/netty/ObjectCreateException.java
@@ -0,0 +1,12 @@
+package com.jd.netty;
+
+/**
+ * @author: <a href="mailto:daixunan@jd.com">戴续楠</a>
+ * @date:2018/11/13
+ */
+public class ObjectCreateException extends RuntimeException {
+
+    public ObjectCreateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/com/jd/netty/TestObjectFactory.java
@@ -0,0 +1,40 @@
+package com.jd.netty;
+
+import org.springframework.util.Assert;
+import org.springframework.validation.ObjectError;
+
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * @author: <a href="mailto:daixunan@jd.com">戴续楠</a>
+ * @date:2018/11/13
+ */
+public class TestObjectFactory<C> implements ObjectFactory<C>{
+
+    private final Class<? extends C> clazz;
+
+    public TestObjectFactory(Class<? extends C> clazz) {
+        Assert.notNull(clazz, "clazz is null When init TestObjectFactory");
+        this.clazz = clazz;
+    }
+
+    @Override
+    public C newObject() {
+        try {
+            return clazz.getConstructor().newInstance();
+        } catch (Throwable e) {
+            throw new ObjectCreateException("create Object failed from TestObjectFactory " + clazz, e);
+        }
+    }
+
+//    @Override
+//    public String toString() {
+//        return StringUtil.simpleClassName(clazz) + ".class";
+//    }
+}
+
+
+
+interface ObjectFactory<T extends Object>{
+    T newObject();
+}
