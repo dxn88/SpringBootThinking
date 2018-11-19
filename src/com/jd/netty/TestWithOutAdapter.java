7ada30d017e1b5d005716389a413348dc00b827d
daixunan <346972070@qq.com>
2018-11-19 15:06:14 +0800

    增加接口默认实现注释

--- src/com/jd/netty/TestWithOutAdapter.java
+++ src/com/jd/netty/TestWithOutAdapter.java
@@ -7,14 +7,14 @@ package com.jd.netty;
 public class TestWithOutAdapter implements caculate{
 }
 
-
+// 接口默认实现，省去了Adapter类的默认实现 java8新增功能
 interface caculate{
     default int add(){ return 0;}
     default int deduce(){ return 0;}
     default int mul(){ return 0;}
 }
 
-// 接口默认实现，省去了Adapter类的默认实现 java8新增功能
+// 下面的不要了
 abstract class TestAdapter implements caculate{
     @Override
     public int add() {
