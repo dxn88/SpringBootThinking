fdd790003f28c9dfa6394427aeb2578cdb2091b5
daixunan <346972070@qq.com>
2019-01-01 16:15:31 +0800

    获取指定的堆栈信息，进行线程优化

--- src/com/jd/TestException.java
+++ src/com/jd/TestException.java
@@ -1,6 +1,9 @@
 package com.jd;
 
 import io.netty.util.internal.EmptyArrays;
+import lombok.SneakyThrows;
+
+import java.lang.reflect.Method;
 
 /**
  * @author: <a href="mailto:daixunan@jd.com">戴续楠</a>
@@ -8,29 +11,56 @@ import io.netty.util.internal.EmptyArrays;
  */
 public class TestException {
     public static void main(String[] args) {
-        try {
-            testException();
-        } catch (RuntimeException e) {
-            System.out.println("e.getMessage() = " + e.getMessage());
-            System.out.println("e= " + e);
-            e.printStackTrace();
-        }
+        MyException myException = new MyException("");
+        myException.printStackTraceDepth();
+        printStack(myException);
     }
 
-    public static void testException() {
-            throw new MyException("参数异常");
+    public static void printStack(MyException myException) {
+        myException.printStackTraceDepth();
     }
+
 }
 
 
 class MyException extends RuntimeException {
+    private  Method fillInStackTrace;
+
+    private  Method getStackTraceDepth;
+
+    private Method getStackTraceElement;
+
     public MyException(String message) {
-        super(message, null, false, false);
+        super(message, null, false, true);
+        init();
     }
 
-//    @Override
-//    public synchronized Throwable fillInStackTrace() {
-////        setStackTrace(EmptyArrays.EMPTY_STACK_TRACE);
-//        return this;
-//    }
+    @SneakyThrows
+    public void init() {
+        fillInStackTrace = Throwable.class.getDeclaredMethod("fillInStackTrace", int.class);
+        fillInStackTrace.setAccessible(true);
+
+        getStackTraceDepth = Throwable.class.getDeclaredMethod("getStackTraceDepth");
+        getStackTraceDepth.setAccessible(true);
+
+        getStackTraceElement = Throwable.class.getDeclaredMethod("getStackTraceElement", int.class);
+        getStackTraceElement.setAccessible(true);
+    }
+    @SneakyThrows
+    public void printStackTraceDepth() {
+
+        Object invokefillInStackTrace = fillInStackTrace.invoke(this,0);
+        System.out.println("invoke = " + invokefillInStackTrace);
+
+        int depth = (int)getStackTraceDepth.invoke(this);
+        System.out.println("invoke = " + depth);
+
+        StackTraceElement invoke1 = (StackTraceElement)getStackTraceElement.invoke(this, depth -2);
+        System.out.println("invoke1.getClassName() = " + invoke1.getClassName());
+        System.out.println("invoke1.getLineNumber() = " + invoke1.getLineNumber());
+        System.out.println("invoke1.getFileName() = " + invoke1.getFileName());
+        System.out.println("invoke1.getMethodName() = " + invoke1.getMethodName());
+
+    }
 }
+
