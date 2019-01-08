28dcafdcc0608912244e846e319d04f023292dda
daixunan <346972070@qq.com>
2019-01-08 11:28:13 +0800

    线上要捕获异常，因为普通的异常只打印到控制台，没法在线上看日志

--- src/com/jd/spiimpl/TestSpi.java
+++ src/com/jd/spiimpl/TestSpi.java
@@ -11,6 +11,7 @@ import java.util.ServiceLoader;
  */
 public class TestSpi {
     public static void main(String[] args) {
+        // 注解方式
         ServiceLoader<HelloService> s = ServiceLoader.load(HelloService.class);
         Iterator<HelloService> searchs = s.iterator();
         if(searchs.hasNext()){
---
+++ src/testNullpointer.java
@@ -0,0 +1,13 @@
+/**
+ * @author: <a href="mailto:daixunan@jd.com">戴续楠</a>
+ * @date:2019/1/8
+ */
+public class testNullpointer {
+    public static void main(String[] args) {
+        print();
+    }
+
+    public static void print() {
+        throw new Error();
+    }
+}
