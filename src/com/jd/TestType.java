a0e55ecd449c53ecc3e3b741d5291f6a00f3a99a
daixunan <346972070@qq.com>
2019-01-08 14:26:53 +0800

    Class中有各种信息包括name是整个名称，classloader等等，有的时候直接存储即可

---
+++ src/com/jd/TestType.java
@@ -0,0 +1,13 @@
+package com.jd;
+
+/**
+ * @author: <a href="mailto:daixunan@jd.com">戴续楠</a>
+ * @date:2019/1/8
+ */
+public class TestType {
+    public static void main(String[] args) {
+        Class<TestType> testTypeClass = TestType.class;
+
+        System.out.println("testTypeClass = " + testTypeClass);
+    }
+}
