c5df22d4d3302862ebd17f74ff67cd713346d947
daixunan <346972070@qq.com>
2019-01-27 15:46:07 +0800

    performance

---
+++ src/com/jd/TestNull.java
@@ -0,0 +1,12 @@
+package com.jd;
+
+/**
+ * @author: <a href="mailto:daixunan@jd.com">戴续楠</a>
+ * @date:2019/1/15
+ */
+public class TestNull {
+
+    public void setNull(){
+
+    }
+}
--- src/com/jd/javaEight/North.java
+++ src/com/jd/javaEight/North.java
@@ -1,6 +1,12 @@
 package com.jd.javaEight;
 
+import jdk.management.resource.ResourceId;
 import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.BiFunction;
 
 /**
  * @author: <a href="mailto:daixunan@jd.com">戴续楠</a>
@@ -8,5 +14,22 @@ import lombok.Data;
  */
 @Data
 public class North {
-    private String name = "nanfang";
+    private volatile String name = "nanfang";
+    private static int i = 5;
+
+    public static void main(String[] args) {
+        ResourceId resourceId = () -> "hello";
+        ResourceId rd = North::getId;
+
+        List<String> list = new ArrayList<>(8);
+        list.add("hello");
+        System.out.println("list.contains(\"hello\") = " + list.contains("hello"));
+
+        BiFunction<String, Integer, Character> sca = String::charAt;
+        System.out.println("sca = " + sca.apply("abce", 2));
+    }
+
+    public static String getId() {
+        return "getIdo";
+    }
 }
